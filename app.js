828c6cd66ba03fd51e22db21d03c79a3e60b8e68
Mudasiru Rasheed <rasheedrtm1@gmail.com>
2020-09-21 19:31:48 +0100

    commit v1.0.0

---
+++ app.js
@@ -0,0 +1,68 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  //   if (req.url === "/") {
+  //     fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+  //       if (err) throw err;
+  //       res.writeHead(200, { "content-Type": "text/html" });
+  //       res.end(data);
+  //     });
+  //   }
+
+  const filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let extname = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+
+    case ".png":
+      contentType = "image/png";
+      break;
+
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "notFound.html"),
+          (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data, "utf8");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server ready on port ${PORT}`);
+});
